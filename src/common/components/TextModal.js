127fb3056e4a7053dbc3f758c7b58874f25067c1
Вячеслав Елизарьев <agnosticgit@gmail.com>
2020-06-12 16:29:02 +0500

    upd

--- src/common/components/TextModal.js
+++ src/common/components/TextModal.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { useDispatch } from 'react-redux'
+import { Text, StyleSheet } from 'react-native';
+import { useDispatch } from 'react-redux';
 import { AuthActions } from '../../modules/auth/redux/AuthActions';
+import { Colors } from '../theme/Colors';
 
 export const TextModal = ({ errors }) => {
   const dispatch = useDispatch()
 
   if (errors) {
-    setTimeout(() => dispatch(AuthActions.setErrors({errors: null})), 2000)
+    setTimeout(() => dispatch(AuthActions.setErrors({ errors: null })), 2000)
   }
 
   return (
-    <Text>{errors}</Text>
+    <Text style={styles.title}>{errors}</Text>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    color: Colors.red
+  }
+})
